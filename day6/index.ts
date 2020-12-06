43a15c7092c6341f993bda2c27907528b2346fc7
ajawaid <ajawaid@convoy.com>
2020-12-05 21:40:49 -0800

    Fix set

--- day6/index.ts
+++ day6/index.ts
@@ -10,8 +10,7 @@ export class P1 implements Solution {
     const inputStrings = input.split("\n\n");
     return inputStrings.map((inputString) => {
       const cleanString = inputString.replace(/\n/g, "");
-      const set = new Set<string>();
-      cleanString.split("").forEach((c) => set.add(c));
+      const set = new Set<string>(cleanString.split(""));
       return {
         correctAnswers: set,
       };
@@ -22,13 +21,11 @@ export class P1 implements Solution {
     const inputStrings = input.split("\n\n");
     return inputStrings.map((inputString) => {
       const allPpl = inputString.split("\n");
-      const allPossibleAnswers = new Set<string>();
       const [firstPerson, ...otherPpl] = allPpl;
-      firstPerson.split("").forEach((c) => allPossibleAnswers.add(c));
+      const allPossibleAnswers = new Set<string>(firstPerson.split(""));
       otherPpl.forEach((answer) => {
         // find out if each answer in this person is also in the first answer
-        const thisPersonsAnswer = new Set<string>();
-        answer.split("").forEach((c) => thisPersonsAnswer.add(c));
+        const thisPersonsAnswer = new Set<string>(answer.split(""));
         Array.from(allPossibleAnswers.values()).forEach((a) => {
           if (!thisPersonsAnswer.has(a)) {
             allPossibleAnswers.delete(a);
